8e7f2767cb34be01b23d15ca3181cafdd201d1c4
veenapk <41325034+veenapk@users.noreply.github.com>
2022-03-25 09:50:55 +0530

    Create tryjava.java

---
+++ tryjava.java
@@ -0,0 +1,6 @@
+class Add{
+  public static void main(String args[]
+                          {
+                            System.out.prinln("Hello this is file for git jenkin integration");
+                          }
+          }
