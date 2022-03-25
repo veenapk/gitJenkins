602e7cef1f81214254a45cbbbdfb5d78e71fc40e
veenapk <41325034+veenapk@users.noreply.github.com>
2022-03-25 09:51:21 +0530

    Update tryjava.java

--- tryjava.java
+++ tryjava.java
@@ -1,5 +1,5 @@
 class Add{
-  public static void main(String args[]
+  public static void main(String args[])
                           {
                             System.out.prinln("Hello this is file for git jenkin integration");
                           }
